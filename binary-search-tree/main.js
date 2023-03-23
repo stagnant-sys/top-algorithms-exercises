4e458ddfd8375a88a40faf73ee7cde5a09724bfd
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-03-23 11:17:39 +0100

    Update main.js

--- binary-search-tree/main.js
+++ binary-search-tree/main.js
@@ -215,17 +215,3 @@ function randomArray(n, upperLimit) {
 
 let testArray = randomArray(50, 100);
 let testTree = Tree(testArray);
-
-
-const prettyPrint = (node, prefix = '', isLeft = true) => {
-  if (node === null) {
-     return;
-  }
-  if (node.right !== null) {
-    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-  }
-  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
-  if (node.left !== null) {
-    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-  }
-};
